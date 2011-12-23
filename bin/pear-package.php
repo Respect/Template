5e8a3c0419cf21be74bcf203e75aa27cda8f851b
Alexandre Gaigalas <alexandre@gaigalas.net>
2011-12-23 17:12:19 -0200

    Trying to fix travis, again

--- bin/pear-package.php
+++ bin/pear-package.php
@@ -1,3 +1,4 @@
+
 <?php
 
 chdir(__DIR__);
@@ -11,11 +12,14 @@ if (!file_exists($package_xml_file))
     die("package.xml does not exists");
 
 $package_data               = simplexml_load_file($package_xml_file);
-$dir_name                   = (string) $package_data->contents->dir['name'];
+$dir_name                   = (string) $package_data->contents->dir->file['name'];
+$dir_name                   = implode('/', array_slice(explode('/', $dir_name), 0, 3)); //not proud of this
 $target                     = realpath("../$dir_name");
 $base_install_dir           = (string) $package_data->contents->dir['baseinstalldir'];
 unset($package_data->contents->dir);
+unset($package_data->phprelease->filelist);
 $main_dir                   = $package_data->contents->addChild('dir');
+$filelist                   = $package_data->phprelease->addChild('filelist');
 $main_dir['name']           = $dir_name;
 $main_dir['baseinstalldir'] = $base_install_dir;
 
@@ -23,12 +27,15 @@ foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($target), 
     if (!$php_file->isFile())
         continue;
     $file                   = $main_dir->addChild('file');
+    $install                = $filelist->addChild('install');
+    $install['as']          = str_replace($target, '', $php_file);
     $file['role']           = 'php';
     $file['baseinstalldir'] = $base_install_dir;
-    $file['install-as']     = str_replace($target, '', $php_file);
-    $file['name']           = $dir_name . $file['install-as'];
+    $file['name']           = $dir_name . $install['as'];
+    $install['name']        = $file['name'];
 }
 
+$package_data->contents->dir['name'] = '/';
 $package_data->date = date('Y-m-d');
 $package_data->time = date('H:i:s');
 $current_version = $package_data->version->release;
@@ -44,6 +51,16 @@ switch ($version_type) {
         $minor_version = 0;
     case 'minor_version':
         $patch_version = 0;
+}
+
+
+foreach ($package_data->changelog->release as $old_release) {
+    if (!$old_release->date)
+        $old_release->addChild('date', '2009-09-09');
+    if (!$old_release->time)
+        $old_release->addChild('time', '09:09:09');
+    if (!$old_release->notes)
+        $old_release->addChild('notes', 'This release date and time might be innacurate.');
 }
 
 $changelog = $package_data->changelog->addChild('release');
@@ -63,6 +80,9 @@ $changelog->stability->api
         = (string) $stability;
 $changelog->license = $package_data->license;
 $changelog->license['uri'] = $package_data->license['uri'];
+$changelog->date = (string) $package_data->date;
+$changelog->time = (string) $package_data->time;
+$changelog->notes = '-';
 
 $dom = new DOMDocument('1.0');
 $dom->preserveWhiteSpace = false;
