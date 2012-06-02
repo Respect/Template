3fd1193fc53807380f2d0212862186f4a171240d
Henrique Moody <henriquemoody@gmail.com>
2012-06-02 12:49:24 -0300

    Update composer dependences and autoloader.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,23 +7,26 @@ date_default_timezone_set('UTC');
  * Autoloader that implements the PSR-0 spec for interoperability between
  * PHP software.
  */
-if (($autoloader = @include __DIR__.'/../vendor/.composer/autoload.php')) {
-    $autoloader->add('Zend', __DIR__ . '/../vendor/pear-zf2/Zend_Dom/php');
-} else {
-    set_include_path('../library' . PATH_SEPARATOR . get_include_path());
+if (!@include __DIR__ . '/../vendor/autoload.php') {
+
+    /* Include path */
+    set_include_path(implode(PATH_SEPARATOR, array(
+        __DIR__ . '/../src',
+        get_include_path(),
+    )));
+
+    /* PEAR autoloader */
     spl_autoload_register(
         function($className) {
-            $fileParts = explode('\\', ltrim($className, '\\'));
-
-            if (false !== strpos(end($fileParts), '_'))
-                array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
-
-            $file = implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
-
+            $filename = strtr($className, '\\', DIRECTORY_SEPARATOR) . '.php';
             foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-                if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file))
-                    return require $path;
+                $path .= DIRECTORY_SEPARATOR . $filename;
+                if (is_file($path)) {
+                    require_once $path;
+                    return true;
+                }
             }
+            return false;
         }
     );
 }
