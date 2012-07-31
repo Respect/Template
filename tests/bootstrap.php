dc9abb7e3669b83c18b6789fc00828c341746464
nickl- <github@jigsoft.co.za>
2012-07-31 06:23:36 +0200

    Updated bootstrap.php and phpunit.xml to latest Foundation.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,40 +1,41 @@
-<?php
-set_error_handler(function () {
-  echo file_get_contents(dirname(__DIR__).'/INSTALL');
-  exit(1);
-}, E_ALL);
+<?php /** a Courtesy of Respect/Foundation */
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-restore_error_handler();
-
-/* Timezone */
 date_default_timezone_set('UTC');
 
-/**
- * Autoloader that implements the PSR-0 spec for interoperability between
- * PHP software.
- */
-if (!@include __DIR__ . '/../vendor/autoload.php') {
+$paths = explode(PATH_SEPARATOR,get_include_path());
+$paths[] = trim(`pear config-get php_dir`);
 
-    /* Include path */
-    set_include_path(implode(PATH_SEPARATOR, array(
-        __DIR__ . '/../src',
-        get_include_path(),
-    )));
-
-    /* PEAR autoloader */
-    spl_autoload_register(
-        function($className) {
-            $filename = strtr($className, '\\', DIRECTORY_SEPARATOR) . '.php';
-            foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-                $path .= DIRECTORY_SEPARATOR . $filename;
-                if (is_file($path)) {
-                    require_once $path;
-                    return true;
-                }
-            }
-            return false;
-        }
-    );
+// See if composer is present then it should be picked up too
+if (file_exists(dirname(__DIR__).'/vendor/composer')) {
+    $map = require dirname(__DIR__).'/vendor/composer/autoload_namespaces.php';
+    foreach ($map as $path)
+        $paths[] = $path;
 }
+
+natsort($paths);
+array_unshift($paths, dirname(__DIR__) .'/library');
+set_include_path(implode(PATH_SEPARATOR, array_unique($paths)));
+
+/** Autoloader that implements the PSR-0 spec for interoperability between PHP software. */
+spl_autoload_register(
+    function($className) {
+        static $composerClassmap;
+        if (!isset($composerClassmap) && file_exists(dirname(__DIR__).'/vendor/composer'))
+               $composerClassmap = require dirname(__DIR__).'/vendor/composer/autoload_classmap.php';
+        // Also consider composer classMap of course
+        if (isset($composerClassmap[$className]))
+            return require $composerClassmap[$className];
+
+	$fileParts = explode('\\', ltrim($className, '\\'));
+
+        if (false !== strpos(end($fileParts), '_'))
+            array_splice($fileParts, -1, 1, explode('_', current($fileParts)));
+
+        $file = implode(DIRECTORY_SEPARATOR, $fileParts) . '.php';
+
+        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+            if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file))
+                return require $path;
+        }
+    }
+);
